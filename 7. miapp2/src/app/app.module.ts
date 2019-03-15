3d3eaceabaf571d27d4c55780cc1eb7a5eaf7429
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-03-15 17:07:16 -0500

    5. Miscelaneos iniciando cap 139

--- 7. miapp2/src/app/app.module.ts
+++ 7. miapp2/src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgStyleComponent } from './components/ng-style/ng-style.component';
-import { CssComponent } from './components/css/css.component';
-import { ClasesComponent } from './components/clases/clases.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NgStyleComponent,
-    CssComponent,
-    ClasesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
